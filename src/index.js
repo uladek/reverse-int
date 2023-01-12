d8a94dc1dd84ea677ccab04a714ee222ee1fc885
Uladimir Hrydzin <hrydzin@gmail.com>
2023-01-12 19:04:03 +0100

    add: solution reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+
+    let reverseN = Math.abs(n).toString().split('');
+    return +reverseN.reverse().join(''); 
+   
 }
+
+// let reverseN = n.toString().split('');
+// return parseFloat(reverseN.reverse().join('')); 
+
+// let reverseN = Math.abs(n).toString().split('');
+// return parseFloat(reverseN.reverse().join('')); 
